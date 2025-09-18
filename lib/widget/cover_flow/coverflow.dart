362282eed95a925bbcb3942d10fa06ed25554446
向宇 <442262740@qq.com>
2025-09-18 20:29:32 +0800

    feat: coverflow组件完善

--- lib/widget/cover_flow/coverflow.dart
+++ lib/widget/cover_flow/coverflow.dart
@@ -2,100 +2,137 @@ library coverflow;
 
 import 'dart:math';
 
+import 'package:bujuan/pages/home/body/body_pages/personal_page.dart';
+import 'package:bujuan/pages/home/bottom_panel/bottom_panel_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_constraintlayout/flutter_constraintlayout.dart';
+import 'package:get/get.dart';
 
 class CoverFlow extends StatefulWidget {
   final List<Widget> images;
-  final Function? onCenterItemSelected;
+  final double cardSize;
+  final bool enableListViewScrolling; // 是否启用ListView式滑动
 
   const CoverFlow({
     super.key,
     required this.images,
-    this.onCenterItemSelected,
+    required this.cardSize,
+    this.enableListViewScrolling = false,  // 默认使用PageView式滑动
   });
 
   @override
   State<CoverFlow> createState() => _CoverFlowState();
 }
-
 class _CoverFlowState extends State<CoverFlow> {
   PageController? _pageController;
-  double _currentPagePosition = 0;
+  ScrollController? _scrollController;
+  double _currentPageIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(initialPage: _currentPagePosition.toInt());
-    _pageController!.addListener(() {
-      setState(() {
-      _currentPagePosition = _pageController!.page ?? 0;
-    });
-    });
+    if (widget.enableListViewScrolling) {
+      _scrollController = ScrollController()
+        ..addListener(() {
+          setState(() {
+            _currentPageIndex = min(_scrollController!.offset / widget.cardSize, widget.images.length - 1) ;
+          });
+        });
+    } else {
+      _pageController = PageController(initialPage: _currentPageIndex.toInt())
+        ..addListener(() {
+          setState(() {
+            _currentPageIndex = _pageController!.page ?? 0;
+          });
+        });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
+        return Stack(
+          children: <Widget>[
+            CoverFlowCardItems(
+              images: widget.images,
+              pagePositon: _currentPageIndex,
+              maxWidth: constraints.maxWidth,
+              maxHeight: constraints.maxHeight,
 
-        double cardSize = constraints.maxHeight;
+              // 卡片尺寸
+              cardSize: widget.cardSize,
+              nearByCardGapWidth: widget.cardSize / 2,
+              farCardGapWidth: widget.cardSize * 2 / 5,
+              // 卡片偏转角度
+              nearByCardAngle: pi / 2 * 1 / 3,
+              farCardAngle: pi / 2 * 2 / 3,
 
-        print("cardSize: $cardSize");
-        print("nearByCardGapWidth: ${cardSize * (2 * cos(pi/2 * 1/3 / 2) - 1)}");
-
+              enableListViewScrolling: widget.enableListViewScrolling,
+            ),
+            // 根据模式选择不同的滚动实现
+            Positioned.fill(
+              child: widget.enableListViewScrolling
+                  ? _buildListViewScrolling(constraints)
+                  : _buildPageViewScrolling(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  // PageView滚动实现
+  Widget _buildPageViewScrolling() {
+    return PageView.builder(
+      itemCount: widget.images.length,
+      controller: _pageController,
+      onPageChanged: (index) {
+        print('PageView onPageChanged: $index');
+      },
+      itemBuilder: (context, index) {
         return GestureDetector(
-          // 左右点击滚动
-          onTapUp: (detail) {
-            final double centerPosition = constraints.maxWidth / 2;
-
-            final double centerCardLeftEdge = centerPosition - cardSize / 2;
-            final double centerCardRightEdge = centerPosition + cardSize / 2;
-
-            if (detail.localPosition.dx <= centerCardLeftEdge) {
-              _pageController!.animateToPage(
-                  _currentPagePosition.toInt() - 1,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeIn
-              );
-            } else if (detail.localPosition.dx >= centerCardRightEdge) {
-              _pageController!.animateToPage(
-                  _currentPagePosition.toInt() + 1,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeIn
-              );
-            } else {
-              widget.onCenterItemSelected!(_currentPagePosition.toInt());
-            }
+          onPanStart: (details) {
+            print('PageView 手势开始: ${details.localPosition}');
           },
-          child: Stack(
-            children: <Widget>[
-              CoverFlowCardItems(
-                images: widget.images,
-                pagePositon: _currentPagePosition,
-                maxWidth: constraints.maxWidth,
-                maxHeight: constraints.maxHeight,
-                cardSize: cardSize,
-                nearByCardAngle: pi/2 * 2/3,
-                nearByCardGapWidth: cardSize * (2 * cos(pi/2 * 2/3 / 2) - 1),
-                // nearByCardGapWidth: cardSize * 3 /5,
-                farCardAngle: pi/2 * 2/3,
-                farCardGapWidth: cardSize * 1/5,
-              ),
-              // 利用空pageview图层来实现滚动功能
-              Positioned.fill(
-                child: PageView.builder(
-                  itemCount: widget.images.length,
-                  controller: _pageController,
-                  itemBuilder: (context, index) {
-                    return Container();
-                  },
-                ),
-              ),
-            ],
+          onPanUpdate: (details) {
+            print('PageView 手势更新: ${details.delta}');
+          },
+          onPanEnd: (details) {
+            print(
+                'PageView 手势结束: velocity=${details.velocity.pixelsPerSecond}');
+          },
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.transparent, // 设为透明，只用于手势检测
           ),
         );
       },
     );
+  }
+  // ListView式滚动实现
+  Widget _buildListViewScrolling(BoxConstraints constraints) {
+    return ScrollConfiguration(
+      behavior: NoGlowScrollBehavior(),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        controller: _scrollController,
+        physics: SnappingScrollPhysics(itemExtent: widget.cardSize),
+        child: SizedBox(
+          width: widget.images.length * widget.cardSize + (constraints.maxWidth - widget.cardSize),
+          child: Container(
+            color: Colors.transparent,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    _scrollController?.dispose();
+    super.dispose();
   }
 }
 
@@ -109,7 +146,7 @@ class CoverFlowCardItems extends StatelessWidget {
   final double nearByCardGapWidth;
   final double farCardAngle;
   final double farCardGapWidth;
-
+  final bool enableListViewScrolling;
 
   const CoverFlowCardItems({
     super.key,
@@ -122,6 +159,7 @@ class CoverFlowCardItems extends StatelessWidget {
     required this.nearByCardGapWidth,
     required this.farCardAngle,
     required this.farCardGapWidth,
+    this.enableListViewScrolling = false,
   });
 
   @override
@@ -134,11 +172,17 @@ class CoverFlowCardItems extends StatelessWidget {
         clipBehavior: Clip.none,
         children: [
           // 左边卡片
-          ...images.sublist(0, currentCenterIndex).map((item) {
+          ...images
+              .sublist(max(0, currentCenterIndex - 6), currentCenterIndex)
+              .map((item) {
             return _buildCard(item);
           }).toList(),
           // 中心卡片及其右边卡片
-          ...images.sublist(currentCenterIndex).reversed.map((item) {
+          ...images
+              .sublist(currentCenterIndex,
+                  min(images.length, currentCenterIndex + 7))
+              .reversed
+              .map((item) {
             return _buildCard(item);
           }).toList(),
         ],
@@ -146,7 +190,7 @@ class CoverFlowCardItems extends StatelessWidget {
     );
   }
 
-  Widget _buildCard(Widget item,) {
+  Widget _buildCard(Widget item) {
     final double leftPosition = _getCardPosition(item);
     final Matrix4 transform = _getTransform(item);
 
@@ -170,9 +214,8 @@ class CoverFlowCardItems extends StatelessWidget {
     int pageIndex = (pagePositon + 0.5).toInt();
 
     bool rightScroll = pageIndex - pagePositon > 0 ? true : false;
-    bool isLeftCard = cardIndex == pageIndex
-        ? !rightScroll
-        : cardIndex < pageIndex;
+    bool isLeftCard =
+        cardIndex == pageIndex ? !rightScroll : cardIndex < pageIndex;
 
     // 页面滑动百分比，左负，右正
     double pageScrollPercentage = (pageIndex - pagePositon).abs();
@@ -185,13 +228,21 @@ class CoverFlowCardItems extends StatelessWidget {
       deltaPosition = rightScroll ? nearByCardGapWidth : -nearByCardGapWidth;
       // 中心左右卡片
     } else if ((cardIndex - pageIndex).abs() == 1) {
-      basePostion = centerCardLeftPostion + (isLeftCard ? -1 : 1) * nearByCardGapWidth;
+      basePostion =
+          centerCardLeftPostion + (isLeftCard ? -1 : 1) * nearByCardGapWidth;
       deltaPosition = isLeftCard
-          ? rightScroll ? nearByCardGapWidth : -farCardGapWidth
-          : rightScroll ? farCardGapWidth : -nearByCardGapWidth;
-    // 其余卡片
+          ? rightScroll
+              ? nearByCardGapWidth
+              : -farCardGapWidth
+          : rightScroll
+              ? farCardGapWidth
+              : -nearByCardGapWidth;
+      // 其余卡片
     } else {
-      basePostion = centerCardLeftPostion + (isLeftCard ? -1 : 1) * (nearByCardGapWidth + farCardGapWidth * ((cardIndex - pageIndex).abs() - 1));
+      basePostion = centerCardLeftPostion +
+          (isLeftCard ? -1 : 1) *
+              (nearByCardGapWidth +
+                  farCardGapWidth * ((cardIndex - pageIndex).abs() - 1));
       deltaPosition = rightScroll ? farCardGapWidth : -farCardGapWidth;
     }
 
@@ -205,9 +256,8 @@ class CoverFlowCardItems extends StatelessWidget {
     int pageIndex = (pagePositon + 0.5).toInt();
 
     bool rightScroll = pageIndex - pagePositon > 0 ? true : false;
-    bool isLeftCard = cardIndex == pageIndex
-        ? !rightScroll
-        : cardIndex < pageIndex;
+    bool isLeftCard =
+        cardIndex == pageIndex ? !rightScroll : cardIndex < pageIndex;
 
     // 页面滑动百分比，左负，右正
     double pageScrollPercentage = (pageIndex - pagePositon).abs();
@@ -218,35 +268,41 @@ class CoverFlowCardItems extends StatelessWidget {
     if (cardIndex == pageIndex) {
       baseAngle = 0;
       deltaAngle = rightScroll ? nearByCardAngle : -nearByCardAngle;
-    // 中心左右卡片
+      // 中心左右卡片
     } else if ((cardIndex - pageIndex).abs() == 1.0) {
       baseAngle = isLeftCard ? -nearByCardAngle : nearByCardAngle;
       deltaAngle = isLeftCard
-          ? rightScroll ? nearByCardAngle : (nearByCardAngle - farCardAngle)
-          : rightScroll ? -(nearByCardAngle - farCardAngle) : -nearByCardAngle;
-    // 中心左右卡片2
+          ? rightScroll
+              ? nearByCardAngle
+              : (nearByCardAngle - farCardAngle)
+          : rightScroll
+              ? -(nearByCardAngle - farCardAngle)
+              : -nearByCardAngle;
+      // 中心左右卡片2
     } else if ((cardIndex - pageIndex).abs() == 2) {
       baseAngle = isLeftCard ? -farCardAngle : farCardAngle;
       deltaAngle = isLeftCard
-          ? rightScroll ? -(nearByCardAngle - farCardAngle) : 0
-          : rightScroll ? 0 : (nearByCardAngle - farCardAngle);
-    // 其余卡片
+          ? rightScroll
+              ? -(nearByCardAngle - farCardAngle)
+              : 0
+          : rightScroll
+              ? 0
+              : (nearByCardAngle - farCardAngle);
+      // 其余卡片
     } else {
       baseAngle = isLeftCard ? -farCardAngle : farCardAngle;
       deltaAngle = 0;
     }
 
     double angle = baseAngle + deltaAngle * pageScrollPercentage;
-    double centerOffset = isLeftCard ? cardSize / 2 : -cardSize/2;
+    double centerOffset = isLeftCard ? cardSize / 2 : -cardSize / 2;
 
     return Matrix4.identity()
       ..translate(-centerOffset)
       ..setEntry(3, 2, 0.001)
       ..rotateY(angle)
-      ..translate(centerOffset)
-    ;
+      ..translate(centerOffset);
   }
-
 
 // Widget _buildReflectionWidget(
 //     double width, double height, int index, Widget child) {
